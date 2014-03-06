37f752ee1ae9e3b042cb16f21ad258b1d166a64c
Ariel Rosado Rivera <ariel.rosado@medicalcardsystem.com>
2014-03-05 20:12:23 -0400

    Add an cocos2d project

---
+++ TinyAstro/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TinyAstro
+//
+//  Created by Betsy Serrano on 3/5/14.
+//  Copyright XaevenCorp 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TinyAstro/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TinyAstro
+//
+//  Created by Betsy Serrano on 3/5/14.
+//  Copyright XaevenCorp 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
