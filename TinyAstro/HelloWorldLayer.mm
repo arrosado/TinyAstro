c381937abcb22220d61f360c463789dba52fdfd3
Ariel Rosado Rivera <ariel_00720@yahoo.com>
2014-03-06 20:06:33 -0400

    Create gradient and noised texture on screen

--- TinyAstro/HelloWorldLayer.mm
+++ TinyAstro/HelloWorldLayer.mm
@@ -21,8 +21,36 @@
     // 2: Call CCRenderTexture:begin
     [renderTexture beginWithClear:bgColor.r g:bgColor.g b:bgColor.b a:bgColor.a];
     
+    self.shaderProgram = [[CCShaderCache sharedShaderCache] programForKey:kCCShader_PositionColor];
+    
+    CC_NODE_DRAW_SETUP();
+    
     // 3: Draw into the texture
-    // You'll add this later
+    float gradientAlpha = 0.7f;
+    CGPoint vertices[4];
+    ccColor4F colors[4];
+    int nVertices = 0;
+    
+    vertices[nVertices] = CGPointMake(0, 0);
+    colors[nVertices++] = (ccColor4F) {0, 0, 0, 0};
+    vertices[nVertices] = CGPointMake(textureWidth, 0);
+    colors[nVertices++] = (ccColor4F) {0, 0, 0, 0};
+    vertices[nVertices] = CGPointMake(0, textureHeight);
+    colors[nVertices++] = (ccColor4F) {0, 0, 0, gradientAlpha};
+    vertices[nVertices] = CGPointMake(textureWidth, textureHeight);
+    colors[nVertices++] = (ccColor4F) {0, 0, 0, gradientAlpha};
+    
+    ccGLEnableVertexAttribs(kCCVertexAttribFlag_Position | kCCVertexAttribFlag_Color);
+    
+    glVertexAttribPointer(kCCVertexAttrib_Position, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+    glVertexAttribPointer(kCCVertexAttrib_Color, 4, GL_FLOAT, GL_FALSE, 0, colors);
+    glBlendFunc(CC_BLEND_SRC, CC_BLEND_DST);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, (GLsizei)nVertices);
+    
+    CCSprite *noise = [CCSprite spriteWithFile: @"Noise.png"];
+    [noise setBlendFunc:(ccBlendFunc){GL_DST_COLOR, GL_ZERO}];
+    noise.position = ccp(textureWidth/2, textureHeight/2);
+    [noise visit];
     
     // 4: Call CCRenderTexture:end
     [renderTexture end];
