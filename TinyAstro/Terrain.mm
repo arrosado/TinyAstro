7daabe4fbb468c725035465a0977e0e54b71934d
Ariel Rosado Rivera <ariel_00720@yahoo.com>
2014-03-06 21:12:28 -0400

    Getting a better hill algorithm

--- TinyAstro/Terrain.mm
+++ TinyAstro/Terrain.mm
@@ -12,15 +12,40 @@
 
 @implementation Terrain
 
--(void)generateHills {
-    CGSize winSize  = [CCDirector sharedDirector].winSize;
-    float x = 0;
+- (void) generateHills {
+    
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    float minDX = 160;
+    float minDY = 60;
+    int rangeDX = 80;
+    int rangeDY = 40;
+    
+    float x = -minDX;
     float y = winSize.height/2;
-    for(int i =0; i < kMaxHillKeyPoints; ++i)
-    {
+    
+    float dy, ny;
+    float sign = 1; // +1 - going up, -1 - going  down
+    float paddingTop = 20;
+    float paddingBottom = 20;
+    
+    for (int i=0; i<kMaxHillKeyPoints; i++) {
         _hillKeyPoints[i] = CGPointMake(x, y);
-        x += winSize.width/2;
-        y = (random() % (int)winSize.height);
+        if (i == 0) {
+            x = 0;
+            y = winSize.height/2;
+        } else {
+            x += rand()%rangeDX+minDX;
+            while(true) {
+                dy = rand()%rangeDY+minDY;
+                ny = y + dy*sign;
+                if(ny < winSize.height-paddingTop && ny > paddingBottom) {
+                    break;
+                }
+            }
+            y = ny;
+        }
+        sign *= -1;
     }
 }
 
